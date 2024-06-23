7c044a2f8a550c7a7412db138db242fb0e112eb0
Muzzammil Ahmed <muzzammilahmad62435@gmail.com>
2024-06-23 23:49:02 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,39 @@
+const reviews= [
+    {
+      id: 1,
+      name: "Dr. Angela Thomas",
+      job: "Artificial Intelligence Specialist",
+      text: "Dr. Angela Thomas is an outstanding AI specialist whose work in machine learning algorithms has revolutionized predictive analytics. Her insightful research and innovative approaches consistently push the boundaries of AI technology.",
+      image: "https://randomuser.me/api/portraits/women/21.jpg"
+    },
+    {
+      id: 2,
+      name : "John Peterson",
+      job: "Cybersecurity Expert",
+      text: "John Peterson is a highly skilled cybersecurity expert with unparalleled expertise in network security. His strategic implementations and risk assessments have significantly bolstered our data protection measures and overall system security.",
+      image: "https://randomuser.me/api/portraits/men/22.jpg"
+    },
+    {
+      id: 3,
+      name: "Lisa Ramirez",
+      job: "Software Development Engineer",
+      text: "Lisa Ramirez is a top-notch software development engineer who excels in creating efficient, scalable, and user-friendly applications. Her dedication and exceptional problem-solving skills drive her to deliver high-quality software solutions.",
+      image: "https://randomuser.me/api/portraits/women/34.jpg"
+    },
+    {
+      id:4,
+      name: "Dr. Michael Lee",
+      job: "Data Scientist",
+      text: "Dr. Michael Lee is a brilliant data scientist whose analytical prowess and innovative methodologies have greatly enhanced our data-driven decision-making processes. His ability to derive meaningful insights from complex data sets is truly impressive.",
+      image: "https://randomuser.me/api/portraits/men/45.jpg"
+    },
+    {
+      id: 5,
+      name: "Emily Chan",
+      job: "IT Project Manager",
+      text: "Emily Chan is an exceptional IT project manager, known for her meticulous planning and adept handling of multifaceted projects. Her leadership and organizational skills ensure seamless execution and delivery of critical IT initiatives.",
+      image: "https://randomuser.me/api/portraits/women/56.jpg"
+    }
+  ]
+
+  export default reviews
