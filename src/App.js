983ee6b0c602f63bceaf7905ed6f2b8b16139f4b
Muzzammil Ahmed <muzzammilahmad62435@gmail.com>
2024-08-09 16:53:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
+// Importing necessary libraries and components for the App
 import "./App.css";
 import Testimonial from "./components/Testimonial";
 import reviews from './data';
 
+// Main component of the app
 function App() {
   return (
    <div className="flex flex-col w-[100vw] h-[100vh] justify-center items-center bg-gray-200">
+     {/* This is the header of the app */}
     <div className="text-center">
       <h1 className="text-4xl font-bold">
         Our Testimonial
       </h1>
+    {/* Including the Testimonials component here */}
       <div className="bg-violet-400 h-[4px] w-1/5 mt-1 mx-auto"></div>
       <Testimonial reviews={reviews}/>
     </div>
